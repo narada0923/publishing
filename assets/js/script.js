20fda6493e1dcb0d28cd953784d552d211f6bf65
Taichir0923 <54131730+Taichir0923@users.noreply.github.com>
2022-03-11 03:46:04 +0800

    ci/bi

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,29 +5,30 @@ class DOM {
     hexPic = document.querySelector("#hexpic");
     slideTextContainer = document.querySelector('.hex__slider__layer');
     mobileNav = document.querySelector('.mobile-navigation');
+    mobileNavItems = document.querySelectorAll('.navItem__item');
     hexPics = [
         {
-            url: "./assets/images/office0.jfif",
+            url: "#",
             title: "창업 지원 사업",
-            path: '#',
+            path: './assets/images/office0.jfif',
             description: "창업 지원을 위한 교육 및 멘토링, 액셀러레이팅,크라우드펀딩, 마케팅 지원, 직/간접투자 등",
         },
         {
-            url: "./assets/images/office1.jfif",
+            url: "#",
             title: "마케팅 사업",
-            path: '#',
+            path: './assets/images/office1.jfif',
             description: "실질적인 성과를 위한 퍼포먼스 마케팅, 콘텐츠 제작, 뉴미디어 마케팅, 서포터즈, 공모전 국내외 셀링 등",
         },
         {
-            url: "./assets/images/office3.jfif",
+            url: "#",
             title: "MICE 사업",
-            path: '#',
+            path: './assets/images/office3.jfif',
             description: "고객 맞춤형 포럼, 세미나, 전시, 축제, 온라인 행사, 설명회 및 상담회 등",
         },
         {
-            url: "./assets/images/office2.jfif",
+            url: "#",
             title: "영상콘텐츠 사업",
-            path: '#',
+            path: './assets/images/office2.jfif',
             description: "바이럴 및 광고 영상, 예능 콘텐츠, 인플루언서 콜라보, 라이브커머스, 비대면 송출 등",
         }
     ];
@@ -54,25 +55,41 @@ class DOM {
         });
     }
 
+    mobileNavToggle() {
+        this.mobileNavItems.forEach(el => {
+            el.classList.remove('active');
+            el.children[1].classList.remove('sublist--active');
+        })
+    }
+
     listenEvents() {
         this.navBtn.addEventListener("click", this.openNavigation.bind(this));
         document.addEventListener("scroll", this.changeHeader.bind(this));
         this.hexNav.forEach((el, index) => {
-            el.style.backgroundImage = `url(${this.hexPics[index].url})`;
+            el.style.backgroundImage = `url(${this.hexPics[index].path})`;
             el.addEventListener("click", () => {
+                const {url , path , title , description} = this.hexPics[index];
                 this.toggleHexSlider();
                 el.classList.add("hex__nav__item--active");
-                this.hexPic.src = this.hexPics[index].url;
+                this.hexPic.src = path;
                 this.slideTextContainer.innerHTML = `
                     <div class="hex__slider__caption">
                         <h2>
-                            <a href=${this.hexPics[index].path}>${this.hexPics[index].title}</a>
+                            <a href=${url}>${title}</a>
                         </h2>
-                        <p>${this.hexPics[index].description}</p>
+                        <p>${description}</p>
                     </div>
                 `
             });
         });
+
+        this.mobileNavItems.forEach((el , index) => {
+            el.addEventListener('click' , () => {
+                this.mobileNavToggle();
+                el.classList.add('active');
+                el.children[1].classList.add('sublist--active')
+            })
+        })
     }
 }
 
